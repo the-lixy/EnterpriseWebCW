b08cbefd7d4ecc1883fc2d88a7314fedfdb271c2
2102582AlexButuc <a.butuc@rgu.ac.uk>
2025-04-07 21:13:34 +0100

    Add page to upload story and page to log in

--- babbleServer.js
+++ babbleServer.js
@@ -11,10 +11,20 @@ app.get('/', function(req, res){
     res.render('pages/homepage');
 });
 
+// submit page
+app.get('/submit', function(req, res){
+    res.render('pages/submit');
+});
+
+// when story is submitted
+app.get('/submittedstory', function(req, res){
+    res.render('pages/submittedstory');
+});
+
+// login page
+app.get('/login', function(req, res){
+    res.render('pages/login');
+});
+
 app.listen(8080);
 console.log("listening on port 8080");
-
-/* app.get('/', function(req, res){
-res.send("Hello world! by express");
-});
-app.listen(8080); */
