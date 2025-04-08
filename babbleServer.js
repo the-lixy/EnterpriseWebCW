8846ad1c93fb2e97dd0da5283640c1571b04de77
2102582AlexButuc <a.butuc@rgu.ac.uk>
2025-04-08 17:40:59 +0100

    Submit story to database works

--- babbleServer.js
+++ babbleServer.js
@@ -3,6 +3,9 @@ var express = require('express');
 var app = express();
 var ejs = require('ejs');
 
+//for using post in forums
+app.use(express.urlencoded({extended:true}))
+
 // load environment variables
 require('dotenv').config(); // Load variables from .env
 
@@ -14,36 +17,53 @@ const { MongoClient } = require('mongodb');
 // environment variable for database security
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
-// try and get first post in stories collection
-async function run() {
-  try {
-    await client.connect();
-    const db = client.db('EnterprizeWebCW');
-    const collection = db.collection('stories');
 
-    // Find the first document in the collection
-    const first = await collection.findOne();
-  } finally {
-    // Close the database connection when finished or an error occurs
-    await client.close();
-  }
-}
-run().catch(console.error);
+let db;
+let collection;
 
-// pages
+(async () => {
+    try {
+      await client.connect();
+      db = client.db('EnterprizeWebCW');
+      collection = db.collection('stories');
+    } catch (err) {
+      console.error("Database connection failed:", err);
+    }
+  })();
+
+// pages --------------------------------------
 
 // home page
-app.get('/', function(req, res){
-    res.render('pages/homepage');
-});
+app.get('/', async(req, res) => {
+    try {
+        const story = await collection.findOne(); // get first story
+        res.render('pages/homepage', { story });
+      } catch (err) {
+        console.error(err);
+        res.status(500).send('Error loading homepage');
+      }
+    });
 
 // submit page
 app.get('/submit', function(req, res){
     res.render('pages/submit');
 });
 
-// when story is submitted
-app.get('/submittedstory', function(req, res){
+// when story is submitted, send it to database
+app.post('/submittedstory', function(req, res){
+    const newstory = {
+        title: req.body.title,
+        story: req.body.story,
+        genre: req.body.genre,
+        author: "Anonymous", // I want to change this later!
+        rating: 0,
+    };
+    
+    db.collection('stories').insertOne(newstory, function(err, result) {
+        if (err) throw err;
+        console.log('saved to database')
+        res.redirect('/')
+        })
     res.render('pages/submittedstory');
 });
 
